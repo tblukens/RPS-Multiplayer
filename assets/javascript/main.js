6a5a1d900f415c3da71eb069195a4d0851dcf4bc
Tim Lukens <tblukens87@gmail.com>
2018-09-25 18:07:56 -0500

    Players names functional and players turns/selections functional

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -20,6 +20,11 @@ var connectedRef = database.ref(".info/connected");
 var playersRef = database.ref("/players");
 // reference for chat in the database
 var chatRef = database.ref("/chat");
+// reference for turn in database
+var turnRef = database.ref("/turn");
+// reference for a queue in database
+var queueRef = database.ref("/queue");
+
 
 
 // using connectedRef to save or remove user from /connections in firebase
@@ -36,17 +41,19 @@ connectedRef.on("value", function (snap) {
 var player1 = null;
 var player2 = null;
 var names = {};
+var playersName = null;
 var player1Wins;
 var player2Wins;
 var player1Losses;
 var player2Losses;
+var turn = 1;
 
 var choices = ['Rock', 'Paper', 'Scissors'];
 
 
 // Game Object
 var rpsGame = {
-    
+
 }
 
 
@@ -71,6 +78,13 @@ $(document).ready(function () {
             player1 = null;
             player1Name = "";
 
+            // if (snapshot.child("player2").exists()) {
+
+            //     $("#set-name").show();
+            // }
+
+            console.log(snapshot.child("player2").exists())
+
         }
 
         // Check for existence of player 2 in the database
@@ -85,12 +99,21 @@ $(document).ready(function () {
             $("#playerTwoName").text(player2Name);
             $("#playerTwoStats").html("Win: " + player2.wins + ", Loss: " + player2.losses + ", Tie: " + player2.ties);
 
+
         } else {
             console.log("Player 2 does NOT exist");
 
+            $("#set-name").show();
             player2 = null;
             player2Name = "";
         }
+        if (snapshot.child("player2").exists() && snapshot.child("player1").exists() === false) {
+            $("#set-name").show();
+        }
+        if (snapshot.child("player2").exists() && snapshot.child("player1").exists() && playersName !== null) {
+            $("#set-name").hide();
+        }
+
     });
     // click function for setting user
     $('#submit').on('click', function (e) {
@@ -99,12 +122,13 @@ $(document).ready(function () {
             console.log("Player 1 added");
 
             // Get users name from input
-            var playersName = $('#your-name').val().trim();
+            playersName = $('#your-name').val().trim();
             // set player information
             player1 = {
                 name: playersName,
                 wins: 0,
                 losses: 0,
+                ties: 0,
                 selection: ""
             }
             // Add player 1 to database
@@ -112,6 +136,9 @@ $(document).ready(function () {
 
             // Removes player if they close browser or refresh
             database.ref("/players/player1").onDisconnect().remove();
+
+            // Set the turn value to 1, as player1 goes first
+            database.ref().child("/turn").set(1);
 
         } else if (player1 !== null && player2 === null) {
             console.log("Added player 2");
@@ -121,6 +148,7 @@ $(document).ready(function () {
                 name: playersName,
                 wins: 0,
                 losses: 0,
+                ties: 0,
                 selection: ""
             }
             database.ref().child("/players/player2").set(player2);
@@ -128,13 +156,85 @@ $(document).ready(function () {
             database.ref("/players/player2").onDisconnect().remove();
         } else if (player1 !== null && player2 !== null) {
 
-            $("#middle-section-name").hide();
-            $("#middle-section-greeting").html("<h3>Sorry you will need to wait for current players to finish before playing. Please refresh the page when one of the players leaves.</h3>")
         }
-
-        $("#middle-section-name").hide();
-
-        $("#middle-section-greeting").show();
+        $("#your-name").val("");
+        $("#set-name").hide();
         return;
     });
+
+    // Listener that detects user disconnection events
+    database.ref("/players/").on("child_removed", function (snapshot) {
+        console.log(snapshot)
+        if (snapshot.key === "player1") {
+            console.log("Player 1 Has Left")
+            $("#playerOneName").text("Player 1");
+            $("#set-name").show();
+        } else if (snapshot.key === "player2") {
+            console.log("Player 2 Has Left")
+            $("#playerTwoName").text("Player 2");
+            $("#set-name").show();
+        }
+        $("#set-name").show();
+    });
+
+    // Monitor Player1's selection
+    $("#player1").on("click", ".choice", function (event) {
+        event.preventDefault();
+
+        // Make selections only when both players are in the game
+        if (player1 && player2 && (playersName === player1.name) && (turn === 1)) {
+            // Record player1's choice
+            var choice = $(this).text().trim();
+            console.log($(this).text().trim())
+
+            // Record the player choice into the database
+            // player1Choice = choice;
+            database.ref().child("/players/player1/selection").set(choice);
+
+            // Set the turn value to 2, as it is now player2's turn
+            turn = 2;
+            database.ref().child("/turn").set(2);
+        }
+    });
+
+    // Monitor Player2's selection
+    $("#player2").on("click", ".choice", function (event) {
+        event.preventDefault();
+
+        // Make selections only when both players are in the game
+        if (player1 && player2 && (playersName === player2.name) && (turn === 2)) {
+            // Record player2's choice
+            var choice = $(this).text().trim();
+
+            // Record the player choice into the database
+            // player2Choice = choice;
+            database.ref().child("/players/player2/selection").set(choice);
+        }
+    });
+
+    // Attach a listener to the database /turn/ node to listen for any changes
+database.ref("/turn/").on("value", function(snapshot) {
+	// Check if it's player1's turn
+	if (snapshot.val() === 1) {
+		console.log("TURN 1");
+		turn = 1;
+
+		// Update the display if both players are in the game
+		if (player1 && player2) {
+			$("#playerPanel1").addClass("yourTurn");
+			$("#playerPanel2").removeClass("yourTurn");
+			$("#middle-section").html("Waiting on " + player1Name + " to choose...");
+		}
+	} else if (snapshot.val() === 2) {
+		console.log("TURN 2");
+		turn = 2;
+
+		// Update the display if both players are in the game
+		if (player1 && player2) {
+			$("#playerPanel1").removeClass("yourTurn");
+			$("#playerPanel2").addClass("yourTurn");
+			$("#middle-section").html("Waiting on " + player2Name + " to choose...");
+		}
+	}
+});
 });
